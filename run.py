9275829fb828ade022c55e1049a50dc6044c2bda
9tailwolf <81214756+9tailwolf@users.noreply.github.com>
2024-11-27 20:36:39 +0900

    Update run.py

--- run.py
+++ run.py
@@ -61,4 +61,5 @@ if __name__ == "__main__":
 
     title = str(today) + ", Papers for " + keywords
     body = generate_paper_list(search, send_limitation, send_term)
+    print(len(body))
     repo.create_issue(title=title, body=body)
